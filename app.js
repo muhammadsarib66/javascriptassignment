73a0d4c3a42a4d3a0da59dc07635df7a76475170
muhammadsarib66 <65099192+muhammadsarib66@users.noreply.github.com>
2021-08-15 00:40:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,401 @@
+
+// CHAPTER NO1
+
+// QUESTION 1
+
+// var a = "welcome to java script world Dear"
+//     alert(a)
+
+//     // QUESTION 2
+
+// var b = "ERROR! Please enter a valid password."
+// alert(b)
+
+// // QUESTION 3
+
+// var c = "Welcome to JS land... " + "  \nHappy Coding!"
+// alert(c)
+
+// // QUESTION 4
+
+// var d = "Welcome to JS land... "  
+// alert(d)
+// var d = "Happy Coding! "  
+// alert(d)
+
+// QUESTION 5
+
+// var e = "Hello.. I can run Js code thorugh my web browser console"
+// console.log(e)
+
+// QUESTION 6
+
+// make use of alert of new html/css  file.?
+// also atteched html file no need to solve this question
+
+// chapter 1 end here 
+
+// Chapter #2
+
+// QUESTION 1
+
+//    var username
+
+// QUESTION 2
+
+// var myname = "sarib noor"
+
+// QUESTION 3
+
+// var message = "Hello World"
+// alert(message)
+
+// QUESTION 4
+
+// var namee = prompt("Enter your name")
+// var age = +prompt("Enter your age")
+// var course = "you are enroll in web and app development"
+// alert(namee)
+// alert(age)
+// alert(course)
+
+
+// Question 5
+
+// var a = "PIZZA"+ "\nPIZZ"+"\nPIZ"+"\nPI"
+// alert(a)
+
+// Question 6
+
+// var string = ("My email address is")
+// var email = ("@saribnoor0310@gmail.com")
+// alert(string.concat(email))
+
+// Question 7
+
+// var BOOKS = ("I am trying to learn from A smarter way to learn JAVASCRIPT")
+// alert(BOOKS)
+
+// Question 8
+
+// var a = ("yeah i can wtite HTML content through Java Script ")
+// document.write(a)
+
+// Question 9
+
+// var a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(a)
+
+// chapter 2 end
+
+// chapter 3
+
+// Question 1 
+
+// var age = ("i am 15 year old")
+// alert(age)
+
+// Question 2
+
+// var visitor = ("you have visited this website 15 times")
+// alert(visitor)
+
+// Question 3
+
+// var birthyear = ("My birth year is "+ 1999)
+// var d = ("\n Data type of my decleared variable is number")
+// document.write(birthyear.concat(d))
+
+// Question 3
+
+// var visitor   = prompt("enter your name")
+// var producttitle  = prompt("prduct tile","Tshirts,pants")
+// var quantity = +prompt("quantity")
+// document.write(visitor + "\n\norderd\n\n"+quantity +producttitle + "\n\non\n\nXYZ clothing store")
+
+
+// CHAPTER 3 END
+
+// CHAPTER 4
+
+//  question 1
+// var one = 1, two = 'two', three= "helloworld"
+// alert(one + two + three)
+
+// question 2
+
+// var $ = ('legal variables')
+// var a = ('legal variables')
+// var variablename ('legal variables')
+// var _var = ('legal variables')
+// var var123 = ('legal variables')
+
+// var 3 = ('illegal variables')
+// var ? = ('illegal variables')
+// var .sdasda = ('illegal variables')
+// var [var] = ('illegal variables')
+
+
+// question 3
+
+
+// var sad = (" <p>RULES FOR NAMING JS VARIABLE </p>" )
+// var a = ( '<p>Variables name can only contain , numbers, $ and _. For example: $my_1stvalue</p> ')
+// var b =("<p>Variable must began with a letter, $ or _ . For Example, _name or name</p>" )
+// var c =("<p> Variable names are case Senesitives</p>")
+// var d =("<p>Variable names should not be JS Keywords</p>" )
+// // alert(sad + a + b)
+// document.write(a + b +c+d)
+
+// var namee = "JAVASCRIPT"
+// document.write("<h1> RULES FOR NAMING,"+ namee +"</h1>" )
+// document.write('<p>Variables name can only contain , numbers, $ and _. For example: $my_1stvalue </p>' )
+// document.write("<p>Variable must began with a letter, $ or _ . For Example, _name or name</p> ")
+// document.write("<p>Variable names are case Senesitives</p>")
+
+//CHAPTER 4 END
+
+//CHAPTER 5 START
+
+// TASK 1
+
+// var a = +prompt("enter the number")
+// var b = +prompt("enter the number")
+// document.write( "sum of " +a +("+")+b +("=") + result )
+
+// TASK 2
+
+// var a = +prompt("enter the number")
+// var b = +prompt("enter the number")
+// document.write( "multipliction of " +a +("*")+b +("=") + result )
+
+// TASK 2
+
+// var a = +prompt("enter the number")
+// var b = +prompt("enter the number")
+// var result = (a/b)
+// document.write( "division of " +a +("/")+b +("=") + result )
+
+// TASK 3
+
+// var a = ('<p>Value after variable declaration is undefined </p>')
+// var b = +prompt("Initialize the variable with some number.")
+// var c = "initial number is " + b++
+// var d = (" <p>Value after increment is = " + b) 
+// var e = 7  
+// var f = (e+b)
+// var g = ("<p> Value after addition is =" + f )
+// var h = --f
+// var i = ("<p>value after decreament is " + h)
+// var j = 3
+// var k = h%j
+// var l = ("<p>the reminder is " + k )
+// document.write(a + c + d + g + i + l)
+
+
+// task 4 
+
+// var a = ("cost of 1 movie ticket is 600rs")
+// var b = 600 
+// var c = +prompt("how many tickets you want to book", "type only number ")
+// var d = b*c
+// document.write("total cost to buy " + c + " ticket is "+ d )
+
+// task 5
+
+// var a = +prompt("which table you want to read", "only Table of 4 is available" )
+// var c = ("<p> 4x1=")+ 4*1
+// var d = ("<p> 4x2=")+ 4*2
+// var e = ("<p> 4x3=")+ 4*3
+// var f = ("<p> 4x4=")+ 4*4
+// var g = ("<p> 4x5=")+ 4*5
+// var h = ("<p> 4x6=")+ 4*6
+// var j = ("<p> 4x7=")+ 4*7
+// var k = ("<p> 4x8=")+ 4*8
+// var l = ("<p> 4x9=")+ 4*9
+
+// document.write(c+d+e+f+g+h+j+k+l)
+
+// Task 6
+// var a = +prompt(" Input °C value to convert in Ferhanite ")
+// var b =  ( a * 9/5 + 32  +" F is " + a + " °C " )  
+// document.write(b)
+
+// var a = +prompt("Input Ferhanite value to convert in °C ")
+// var d = (a-32) * 5/9 + " °C is " + a + "F"
+// document.write(d)
+
+
+// task 7
+// var a = +prompt("type price of item")
+// var b = ("<p> price of item 1 is ") + a
+// var c = +prompt("how much quantity")
+// var d = ("<p> quantity of item 1 is ") + c
+// var e = +prompt("type price of item 2")
+// var f = ("<p> price of item 2 is ") + e
+// var g = +prompt("how much quantity")
+// var h = ("<p> quantity of item 2 is") + g
+// var l = 100
+// var i = ("<p> shipping charger is 100 ")
+// var t = (a * c) + 100 + (e * g)
+// var j = ("<p> total cost of your order is ") + t
+// document.write(b + d + f + h + i + j)
+
+
+// task 8
+//  var a = ("<h1> MARK SHEET</h1>")
+// var b = +prompt("total marks of your exam")
+// var e = "TOTAL MARKS " + b
+// var d = +prompt("obtain marks")
+// var c = "MARKS OBTAINED " + d
+// var g = d/b*100  
+// var t =  ("percent is") + g
+//  document.write(a + e + c + t)
+
+// task 9
+
+// var a = 150
+// var n = 30
+// var b = +prompt(" currency converter $ to PKR")
+// var d = a*b
+// var c = +prompt(" currency converter Riyal to PKR")
+// var e = n*c
+// var f = d+e
+// document.write("total currecy in pkr " + f)
+
+// tsk 10
+
+// var a = +prompt("enter some number ")
+// var  b = a+5
+// var c = b*10
+// var d = c/2
+// document.write(d)
+
+// task # 11
+
+// task12
+
+// var a = +prompt("enter current year")
+// var b = +prompt("your birth year")
+// var c= a-b
+// document.write("you are " + c + "year old")
+
+// task 13 
+
+// var r = +prompt( "circle radius")
+// var b = "<p> radius of a circle is " + r
+// var a =  2*3.145*r
+// var t = "<p> the circumeference is" + a 
+//  var s = a**2   
+// var d = "<p> area is " + s 
+// document.write(b + t + d)
+
+
+// chapter 5 started 
+
+
+// question 1
+
+// var a = +prompt(" enter your desire ")
+// var r = ("<p>RESULT")
+// var b = ("<p>the value of a is ") + a
+// var q = ("<p> -----------------------------")
+// var c = ++a
+// var d = ("<p>the value of ++a is = " ) + a
+// var s = ("<p>Now the value of a is = ") + a
+// var e = a++
+// var f = ("<p>the value of a++ is = " ) + a
+// var w = ("<p>Now the value of a is = ") + a
+// var z = --a
+// var x = ("<p>the value of --a is = " ) + a
+// var v = ("<p>Now the value of a is = ") + a
+// document.write(r + b + q + b + d + s + f + w + x + v)
+
+// task 3 
+//  var user = prompt("ENter your name")
+// var result = ("<h1>Welcome to Java script World  mr " ) + user
+// document.write(result)
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is false");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is false");
+// }
+
+// IS TASK ME CONDITION BTANI THI K GIVIN STAEMENT SAHI HAI YA NHI 
+// task 3
+// var a = prompt("enter your city name ")
+// if(a = "karachi" ){
+//     alert( "welcome to City of Light")
+// }
+
+//  chapteer 9 to 11
+
+// task 4
+//  var gender = prompt("enter your gender")
+// if(gender = "male" ){
+//     alert("GOOD Morning Sir")
+// }
+// // var gender = prompt("enter your gender")
+// else if (gender = "female" ){
+//     alert("GOOD Morning MAm")}
+
+// task 5 
+
+// var color = prompt("Enter signal color ", " RED, GREEN, BLUE")
+// var a = ("red")
+// var b = ("green")
+// var c = ("blue")
+// if (color == a) {
+//     alert("plz stop")
+// }
+// if (color == b) {
+//     alert("ye watan hamara hai")
+// }
+// if (color == c) {
+//     alert("yo can go")
+// }
+// TASK 6 
+
+// TASK 7
+
+// var a = prompt("guess the scret number from 1 to 10" )
+// var b = 5 
+// if (a == b){
+//     alert("BINGO U GOT IT")
+// }
+// if (a != b){
+//     alert("YOU LOOSER")
+// }
+
+// task 8
+
+// TASK 9
+// var a = +prompt("enter  any number")
+// if ( a % 2 == 0){
+//     alert("even")
+// }
+// else{
+//     alert("odd")
+// } 
+
+// chapter12 to 13
+
+// task 1
+
